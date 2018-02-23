d2d5cb4e6d5a997a0a55f09108bf10e2296f27a1
Oscar Rengifo <oscarrengifo089@gmail.com>
2018-02-22 19:04:00 -0500

    diseñamos el splash pero sin funcionalidad

--- Splash.js
+++ Splash.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, StatusBar } from 'react-native';
+import { Text, View, StyleSheet, StatusBar, Image } from 'react-native';
 
 export default class Splash extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <StatusBar
-     backgroundColor="#00675b"
-     barStyle="light-content"
-     />
-      <Text>Hello world!</Text>
+      <StatusBar backgroundColor="#75a478" barStyle="light-content"/>
+      <Image style={styles.logo} source={require('./logo.png')} />
       </View>
+
     );
   }
 }
@@ -20,7 +18,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#009688',
+    backgroundColor: '#a5d6a7',
+  },
+  logo:{
+    width: 280,
+    height: 280,
   },
   welcome: {
     fontSize: 20,
